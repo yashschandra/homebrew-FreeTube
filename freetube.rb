1ad6fa52dce776c0851602163aead364572e0b46
YASH CHANDRA <yashschandra@gmail.com>
2024-07-16 08:39:52 +0530

    urls correction

--- freetube.rb
+++ freetube.rb
@@ -1,7 +1,7 @@
 class Freetube < Formula
   desc "Watch YouTube videos without ads"
-  homepage "<https://github.com/yashschandra/FreeTube>"
-  url "<https://github.com/yashschandra/FreeTube/releases/download/v0.1/freetube-macos-arm64.tar.gz>"
+  homepage "https://github.com/yashschandra/FreeTube"
+  url "https://github.com/yashschandra/FreeTube/releases/download/v0.1/freetube-macos-arm64.tar.gz"
   sha256 "a3175f4dce0ab70420097fe08fcd8d72a7fdc21764693316d2871c39806c33f3"
   license "MIT"
   version "0.1"
