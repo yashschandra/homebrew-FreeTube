a92446f370b79679bcadece23499e7e45ef899c3
YASH CHANDRA <yashschandra@gmail.com>
2025-02-09 18:14:23 +0530

    0.2 release sha256 update

--- freetube.rb
+++ freetube.rb
@@ -2,7 +2,7 @@ class Freetube < Formula
   desc "Watch YouTube videos without ads"
   homepage "https://github.com/yashschandra/FreeTube"
   url "https://github.com/yashschandra/FreeTube/releases/download/v0.2/freetube-macos-arm64.tar.gz"
-  sha256 "ba4ef2c61622cb768952ed79356b47c2e036014ebeba21953efebfe042925044"
+  sha256 "30acb091cb107bea02a849ce97e5d001fa36ad08d5f6b92ce5824756ec94e5bd"
   license "MIT"
   version "0.2"
 
